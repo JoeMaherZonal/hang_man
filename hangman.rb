77015b3e07d2336a42595c869d0ae7ed2f4258c2
Joseph.Maher <joseph-maher@hotmail.co.uk>
2016-06-15 14:35:14 +0100

    game working

--- hangman.rb
+++ hangman.rb
@@ -1,1 +1,16 @@
 require_relative('game')
+
+hangman = Game.new("star wars")
+
+lives = 5
+
+while lives > 0
+  guess = hangman.next_turn
+
+  if hangman.is_guess_correct(guess)
+    hangman.update_answers_so_far(guess)
+  else
+    lives -= 1
+  end
+  puts hangman.display_answers_so_far
+end
